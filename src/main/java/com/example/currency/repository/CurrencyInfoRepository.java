314043ffac79d8d1be15e19e08e0a5e603d8c031
كيريل <adum5679@gmail.com>
2025-05-31 12:58:47 +0300

    Update CurrencyInfoRepository.java

--- src/main/java/com/example/currency/repository/CurrencyInfoRepository.java
+++ src/main/java/com/example/currency/repository/CurrencyInfoRepository.java
@@ -4,9 +4,7 @@ import com.example.currency.models.CurrencyInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Repository interface for CurrencyInfo entities.
- */
+
 @Repository
 public interface CurrencyInfoRepository extends JpaRepository<CurrencyInfo, Integer> {
 }
